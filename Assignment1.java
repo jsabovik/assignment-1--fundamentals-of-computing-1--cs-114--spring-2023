a5474dc61805682724852f99210a8fafcfe32062
John Sabovik <johnrs1604@gmail.com>
2023-02-06 15:41:39 -0500

    Update Assignment1.java
    steps 1-3

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,30 @@
 // Import libraries here
-
+import java.util.Scanner;
 public class Assignment1 {
     public static void main(String[] args) {
-        // John Sabovik
+        // John Sabovik document and make comments
+        printInitials();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int fDegrees = input.nextInt();
+        int cDegrees = (fDegrees - 32) * (5/9);
+
+        System.out.print("Enter a five character string:");
+        String strIn = input.next();
+        strIn = strIn.substring(1,4);
+
     }
+
+    public static void printInitials() {
+      System.out.println("JJJJJJJJJJJJ      SSSSSSSSSSS");
+      System.out.println("     JJJJ         SSSS");
+      System.out.println("     JJJJ         SSSS");
+      System.out.println("     JJJJ         SSSSSSSSSSS");
+      System.out.println("JJJ  JJJJ                SSSS");
+      System.out.println("JJJ  JJJJ                SSSS");
+      System.out.println("JJJJJJJJJ         SSSSSSSSSSS");
+    }
+
 }
